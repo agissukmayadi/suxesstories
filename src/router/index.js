1db7039928df464bafc2dce9c9727a022e4df213
Selinadita <selinacahyani03@gmail.com>
2024-12-05 09:14:33 +0700

    Update index.js
    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -5,163 +5,185 @@ import { getFirestore, doc, getDoc } from "firebase/firestore";
 import Dashboard from "../views/Dashboard.vue";
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes: [
-    {
-      path: "/",
-      component: Dashboard,
-      meta: {
-        requiresAuth: true,
-        requiresRole: ["admin"],
-      },
-    },
-    {
-      path: "/schedule",
-      component: () => import("../views/Schedule.vue"),
-      meta: {
-        requiresAuth: true,
-        requiresRole: ["admin"],
-      },
-    },
-    {
-      path: "/event",
-      component: () => import("../views/Event.vue"),
-      meta: {
-        requiresAuth: true,
-        requiresRole: ["admin"],
-      },
-    },
-    {
-      path: "/event/create",
-      component: () => import("../views/CreateEvent.vue"),
-      meta: {
-        requiresAuth: true,
-        requiresRole: ["admin"],
-      },
-    },
-    {
-      path: "/company",
-      component: () => import("../views/Company.vue"),
-      meta: {
-        requiresAuth: true,
-        requiresRole: ["admin"],
-      },
-    },
-    {
-      path: "/company/edit/:id",
-      component: () => import("../views/CompanyEdit.vue"),
-      meta: {
-        requiresAuth: true,
-        requiresRole: ["admin"],
-      },
-    },
-    {
-      path: "/user-management",
-      component: () => import("../views/User.vue"),
-      meta: {
-        requiresAuth: true,
-        requiresRole: ["admin"],
-      },
-    },
-    {
-      path: "/talent-dashboard",
-      component: () => import("../views/TalentDashboard.vue"),
-      meta: {
-        requiresAuth: true,
-        requiresRole: ["talent"],
-      },
-    },
-    {
-      path: "/qrcode",
-      component: () => import("../views/QrCode.vue"),
-      meta: {
-        requiresAuth: true,
-        requiresRole: ["talent"],
-      },
-    },
-    {
-      path: "/assign-event/:id",
-      component: () => import("../views/AssignEvent.vue"),
-      meta: {
-        requiresAuth: true,
-        requiresRole: ["talent"],
-      },
-    },
-    {
-      path: "/formdata",
-      component: () => import("../views/FormData.vue"),
-      meta: {
-        requiresAuth: true,
-        requiresRole: ["talent"],
-      },
-    },
-    {
-      path: "/payment",
-      component: () => import("../views/Payment.vue"),
-      meta: {
-        requiresAuth: true,
-        requiresRole: ["talent"],
-      },
-    },
-    {
-      path: "/success",
-      component: () => import("../views/Success.vue"),
-      meta: {
-        requiresAuth: true,
-        requiresRole: ["talent"],
-      },
-    },
-    {
-      path: "/unauthorized",
-      component: () => import("../views/Unauthorized.vue"),
-    },
-    {
-      path: "/login",
-      component: () => import("../views/Auth/Login.vue"),
-      meta: {
-        requiresGuest: true,
-      },
-    },
-  ],
+    history: createWebHashHistory(),
+    routes: [{
+            path: "/",
+            component: Dashboard,
+            meta: {
+                requiresAuth: true,
+                requiresRole: ["admin"],
+            },
+        },
+        {
+            path: "/schedule",
+            component: () =>
+                import ("../views/Schedule.vue"),
+            meta: {
+                requiresAuth: true,
+                requiresRole: ["admin"],
+            },
+        },
+        {
+            path: "/event",
+            component: () =>
+                import ("../views/Event.vue"),
+            meta: {
+                requiresAuth: true,
+                requiresRole: ["admin"],
+            },
+        },
+        {
+            path: "/event/create",
+            component: () =>
+                import ("../views/CreateEvent.vue"),
+            meta: {
+                requiresAuth: true,
+                requiresRole: ["admin"],
+            },
+        },
+        {
+            path: "/company",
+            component: () =>
+                import ("../views/Company.vue"),
+            meta: {
+                requiresAuth: true,
+                requiresRole: ["admin"],
+            },
+        },
+        {
+            path: "/talent",
+            component: () =>
+                import ("../views/Talent.vue"),
+            meta: {
+                requiresAuth: true,
+                requiresRole: ["admin"],
+            },
+        },
+        {
+            path: "/company/edit/:id",
+            component: () =>
+                import ("../views/CompanyEdit.vue"),
+            meta: {
+                requiresAuth: true,
+                requiresRole: ["admin"],
+            },
+        },
+        {
+            path: "/user-management",
+            component: () =>
+                import ("../views/User.vue"),
+            meta: {
+                requiresAuth: true,
+                requiresRole: ["admin"],
+            },
+        },
+        {
+            path: "/talent-dashboard",
+            component: () =>
+                import ("../views/TalentDashboard.vue"),
+            meta: {
+                requiresAuth: true,
+                requiresRole: ["talent"],
+            },
+        },
+        {
+            path: "/qrcode",
+            component: () =>
+                import ("../views/QrCode.vue"),
+            meta: {
+                requiresAuth: true,
+                requiresRole: ["talent"],
+            },
+        },
+        {
+            path: "/assign-event/:id",
+            component: () =>
+                import ("../views/AssignEvent.vue"),
+            meta: {
+                requiresAuth: true,
+                requiresRole: ["talent"],
+            },
+        },
+        {
+            path: "/formdata",
+            component: () =>
+                import ("../views/FormData.vue"),
+            meta: {
+                requiresAuth: true,
+                requiresRole: ["talent"],
+            },
+        },
+        {
+            path: "/payment",
+            component: () =>
+                import ("../views/Payment.vue"),
+            meta: {
+                requiresAuth: true,
+                requiresRole: ["talent"],
+            },
+        },
+        {
+            path: "/success",
+            component: () =>
+                import ("../views/Success.vue"),
+            meta: {
+                requiresAuth: true,
+                requiresRole: ["talent"],
+            },
+        },
+        {
+            path: "/unauthorized",
+            component: () =>
+                import ("../views/Unauthorized.vue"),
+        },
+        {
+            path: "/login",
+            component: () =>
+                import ("../views/Auth/Login.vue"),
+            meta: {
+                requiresGuest: true,
+            },
+        },
+    ],
 });
 
 router.beforeEach((to, from, next) => {
-  const auth = getAuth();
-  const db = getFirestore();
-  const requiresAuth = to.meta.requiresAuth;
-  const requiresGuest = to.meta.requiresGuest;
-  const requiresRole = to.meta.requiresRole;
+    const auth = getAuth();
+    const db = getFirestore();
+    const requiresAuth = to.meta.requiresAuth;
+    const requiresGuest = to.meta.requiresGuest;
+    const requiresRole = to.meta.requiresRole;
 
-  const waitForAuth = () =>
-    new Promise((resolve) => {
-      onAuthStateChanged(auth, (user) => {
-        resolve(user);
-      });
-    });
+    const waitForAuth = () =>
+        new Promise((resolve) => {
+            onAuthStateChanged(auth, (user) => {
+                resolve(user);
+            });
+        });
 
-  waitForAuth().then(async (user) => {
-    if (requiresAuth && !user) {
-      next("/login");
-    } else if (requiresGuest && user) {
-      next("/");
-    } else if (user && requiresRole) {
-      const userDoc = await getDoc(doc(db, "users", user.uid));
-      if (userDoc.exists()) {
-        const userData = userDoc.data();
-        const userRole = userData.role;
+    waitForAuth().then(async(user) => {
+        if (requiresAuth && !user) {
+            next("/login");
+        } else if (requiresGuest && user) {
+            next("/");
+        } else if (user && requiresRole) {
+            const userDoc = await getDoc(doc(db, "users", user.uid));
+            if (userDoc.exists()) {
+                const userData = userDoc.data();
+                const userRole = userData.role;
 
-        if (requiresRole.includes(userRole)) {
-          next();
+                if (requiresRole.includes(userRole)) {
+                    next();
+                } else {
+                    next("/unauthorized");
+                }
+            } else {
+                next("/login");
+            }
         } else {
-          next("/unauthorized");
+            next();
         }
-      } else {
-        next("/login");
-      }
-    } else {
-      next();
-    }
-  });
+    });
 });
 
 export default router;
