8aa3d65de643665c83d84301d9447740e31ec68f
Selinadita <selinacahyani03@gmail.com>
2024-12-13 11:45:10 +0700

    Update Talent.vue
    Update Talent.vue

--- src/views/Talent.vue
+++ src/views/Talent.vue
@@ -1,15 +1,13 @@
 <template>
-    <div class="container py-4">
+    <div class="talent container">
         <h3>Talent</h3>
 
         <!-- Search and Filter Section -->
-        <div class="row mb-3">
+        <div class="row mb-4">
             <div class="col-md-10">
                 <div class="input-group">
-                    <!-- Input Field -->
-                    <input type="text" class="form-control" v-model="search" placeholder="Search" />
-                    <!-- Icon Search di sebelah kanan -->
-                    <span class="input-group-text" style="background-color: #265c7f">
+                    <input type="text" class="form-control" v-model="search" placeholder="Search by name or email" />
+                    <span class="input-group-text">
                         <i class="bi bi-search text-white"></i>
                     </span>
                 </div>
@@ -21,127 +19,71 @@
             </div>
         </div>
 
-
         <!-- Filter Section -->
         <div v-if="showFilter" class="row mb-4">
-            <div class="col-12 col-md-4">
-                <select class="form-select mb-2" v-model="selectedEvent">
-                    <option value="" disabled selected>Select Event</option>
+            <div class="col-sm-6">
+                <p class="mb-1">Selected Event</p>
+                <select class="form-select" v-model="selectedEvent">
                     <option value="Physics Happy Day">Physics Happy Day</option>
                     <option value="Women Strong">Women Strong</option>
                     <option value="Insights Quest">Insights Quest</option>
                 </select>
             </div>
-            <div class="col-12 col-md-4">
-                <select class="form-select mb-2" v-model="selectedTest">
-                    <option value="" disabled selected>Select Test</option>
-                    <option value="Negotiation Skill Test">Negotiation Skill Test</option>
+            <div class="col-sm-6">
+                <p class="mb-1">Selected Test</p>
+                <select class="form-select" v-model="selectedTest">
+                    <option value="5pf tipologi">5pf tipologi</option>
                     <option value="CFIT 3">CFIT 3</option>
-                    <option value="5pf Tipologi">5pf Tipologi</option>
-                </select>
-            </div>
-            <div class="col-12 col-md-4">
-                <select class="form-select" v-model="selectedCompany">
-                    <option value="" disabled selected>Select Company</option>
-                    <option value="PT. Cipta Semesta">PT. Cipta Semesta</option>
-                    <option value="PT. Cipta Angkasa">PT. Cipta Angkasa</option>
-                    <option value="Yayasan Peduli Lansia">Yayasan Peduli Lansia</option>
+                    <option value="Negotiation Skill Test">Negotiaton Skill Test</option>
+                    <option value="Achievement Motivation Profile">Achievement Motivation Profile</option>
+                    <option value="Goal Setting Test">Goal Setting Test</option>
+                    <option value="Scholastic Aptitudes Questionnaire">Scholastic Aptitudes Questionnaire</option>
+                    <option value="Personality 5.0">Personality 5.0</option>
+                    <option value="Creative Problem Solving">Creative Problem Solving</option>
+                    <option value="Emotional Intelligence Test">Emotional Intelligence Test</option>
+                    <option value="Teaching Style Profile">Teaching Style Profile</option>
                 </select>
             </div>
         </div>
 
-        <!-- Talent Table -->
+        <!-- Table Section -->
         <div class="table-responsive">
-            <table class="table table-striped table-hover">
+            <table class="table table-striped table-hover text-center">
                 <thead>
                     <tr>
+                        <th>Download</th>
                         <th>Nama</th>
-                        <th>Email</th>
                         <th>Event</th>
                         <th>Test</th>
-                        <th>Download</th>
+                        <th>Email</th>
+                        <th>No Handphone</th>
+                        <th>Tanggal Lahir</th>
+                        <th>Kota Domisili</th>
+                        <th>Jenis Kelamin</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr v-for="talent in filteredTalents" :key="talent.email">
-                        <td>{{ talent.name }}</td>
-                        <td>{{ talent.email }}</td>
-                        <td>{{ talent.event }}</td>
                         <td>
-                            <a href="#" @click.prevent="showTestModal(talent)" class="text" style="color:#265c7f;">
-                                Lihat Hasil Test
-                            </a>
-                        </td>
-                        <td>
-                            <button class="btn btn-link text" style="color:#265c7f;" @click="showDownloadModal(talent)">
-                                Download
+                            <button class="btn btn-link" style="background-color: #265c7f; color: white;"
+                                @click="downloadPDF(talent)">
+                                <i class="bi bi-download"></i>
                             </button>
                         </td>
+                        <td>{{ talent.name }}</td>
+                        <td>{{ talent.event }}</td>
+                        <td>{{ talent.test }}</td>
+                        <td>{{ talent.email }}</td>
+                        <td>{{ talent.phone }}</td>
+                        <td>{{ talent.birthdate }}</td>
+                        <td>{{ talent.city }}</td>
+                        <td>{{ talent.gender }}</td>
+                    </tr>
+                    <tr v-if="filteredTalents.length === 0">
+                        <td colspan="9" class="text-center">No data found</td>
                     </tr>
                 </tbody>
             </table>
-        </div>
-
-
-        <!-- Test Modal -->
-        <div class="modal fade" id="testModal" tabindex="-1">
-            <div class="modal-dialog modal-lg">
-                <div class="modal-content">
-                    <div class="modal-header btn-primary text-white">
-                        <h5 class="modal-title">Hasil test</h5>
-                        <button type="button" class="btn-close text-white" data-bs-dismiss="modal"
-                            aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <p>Lihat Hasil Test</p>
-                        <div class="d-grid gap-2">
-                            <button class="btn btn-primary text-white d-flex justify-content-between" type="button">
-                                5pf Tipologi <span class="ms-2"> &gt; </span>
-                            </button>
-                            <button class="btn btn-primary text-white d-flex justify-content-between" type="button">
-                                5pf Tipologi <span class="ms-2"> &gt; </span>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Download Modal -->
-        <div class="modal fade" id="downloadModal" tabindex="-1">
-            <div class="modal-dialog modal-lg">
-                <div class="modal-content">
-                    <div class="modal-header btn-primary text-white">
-                        <h5 class="modal-title">Download</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="form-check d-flex justify-content-between align-items-center">
-                            <label class="form-check-label" for="checkAll">
-                                Download Semua
-                            </label>
-                            <input class="form-check-input ms-2" type="checkbox" id="checkAll" v-model="selectAll"
-                                @change="toggleSelectAll" />
-                        </div>
-                        <div class="form-check mt-2 d-flex justify-content-between align-items-center"
-                            v-for="(file, index) in files" :key="index">
-                            <label class="form-check-label" :for="'file' + index">
-                                {{ file }}
-                            </label>
-                            <input class="form-check-input ms-2" type="checkbox" :id="'file' + index"
-                                v-model="selectedFiles" :value="file" />
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button class="btn btn-secondary" data-bs-dismiss="modal">
-                            Cancel
-                        </button>
-                        <button class="btn btn-primary" @click="downloadFiles">
-                            Download PDF
-                        </button>
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
 </template>
@@ -154,25 +96,56 @@ export default {
             showFilter: false,
             selectedEvent: "",
             selectedTest: "",
-            selectedCompany: "",
-            selectedTalent: {},
-            selectAll: false,
-            selectedFiles: [],
-            files: ["5pf Tipologi", "Negotiation Skill Test", "CFIT 3"],
             talents: [
                 {
                     name: "Suci Amalia",
+                    birthdate: "1995-08-25",
+                    city: "Surabaya",
+                    phone: "08123456789",
+                    gender: "Perempuan",
                     email: "admin@gmail.com",
                     event: "Insights Quest",
                     test: "Negotiation Skill Test",
-                    company: "PT. Cipta Semesta",
                 },
                 {
                     name: "Admin",
+                    birthdate: "1992-03-15",
+                    city: "Jakarta",
+                    phone: "08129876543",
+                    gender: "Laki-laki",
                     email: "admin2@gmail.com",
                     event: "Insights Quest",
                     test: "CFIT 3",
-                    company: "PT. Cipta Angkasa",
+                },
+                {
+                    name: "Selina Dita",
+                    birthdate: "2004-04-15",
+                    city: "Surabaya",
+                    phone: "08129876543",
+                    gender: "Perempuan",
+                    email: "admin3@gmail.com",
+                    event: "Women Strong",
+                    test: "Creative Problem Solving",
+                },
+                {
+                    name: "Selina Dita",
+                    birthdate: "2004-04-15",
+                    city: "Surabaya",
+                    phone: "08129876543",
+                    gender: "Perempuan",
+                    email: "admin3@gmail.com",
+                    event: "Physics Happy Day",
+                    test: "Scholastic Aptitudes Questionnaire",
+                },
+                {
+                    name: "Selina Dita",
+                    birthdate: "2004-04-15",
+                    city: "Surabaya",
+                    phone: "08129876543",
+                    gender: "Perempuan",
+                    email: "admin3@gmail.com",
+                    event: "Physics Happy Day",
+                    test: "Teaching Style Profile",
                 },
             ],
         };
@@ -184,13 +157,10 @@ export default {
                     !this.search ||
                     talent.name.toLowerCase().includes(this.search.toLowerCase()) ||
                     talent.email.toLowerCase().includes(this.search.toLowerCase());
-                const matchesEvent =
-                    !this.selectedEvent || talent.event === this.selectedEvent;
+                const matchesEvent = !this.selectedEvent || talent.event === this.selectedEvent;
                 const matchesTest = !this.selectedTest || talent.test === this.selectedTest;
-                const matchesCompany =
-                    !this.selectedCompany || talent.company === this.selectedCompany;
 
-                return matchesSearch && matchesEvent && matchesTest && matchesCompany;
+                return matchesSearch && matchesEvent && matchesTest;
             });
         },
     },
@@ -198,27 +168,24 @@ export default {
         toggleFilter() {
             this.showFilter = !this.showFilter;
         },
-        showTestModal(talent) {
-            this.selectedTalent = talent;
-            const testModal = new bootstrap.Modal(document.getElementById("testModal"));
-            testModal.show();
-        },
-        showDownloadModal(talent) {
-            this.selectedTalent = talent;
-            const downloadModal = new bootstrap.Modal(document.getElementById("downloadModal"));
-            downloadModal.show();
-        },
-        toggleSelectAll() {
-            this.selectedFiles = this.selectAll ? [...this.files] : [];
-        },
-        downloadFiles() {
-            alert("Downloading: " + this.selectedFiles.join(", "));
+        downloadPDF(talent) {
+            const content = `Talent Information\n\nName: ${talent.name}\nEvent: ${talent.event}\nTest: ${talent.test}\nEmail: ${talent.email}\nPhone: ${talent.phone}\nBirthdate: ${talent.birthdate}\nCity: ${talent.city}\nGender: ${talent.gender}`;
+            const blob = new Blob([content], { type: 'application/pdf' });
+            const link = document.createElement('a');
+            link.href = window.URL.createObjectURL(blob);
+            link.download = `${talent.name}_info.pdf`;
+            link.click();
         },
     },
 };
 </script>
 
 <style scoped>
+.btn-link {
+    background-color: #265c7f;
+    color: white;
+}
+
 .input-group-text {
     background-color: #265c7f;
 }
@@ -227,9 +194,5 @@ export default {
     background-color: #265c7f;
     border: none;
 }
-
-.btn-1 {
-    background-color: #265c7f;
-    color: white;
-}
 </style>
+
